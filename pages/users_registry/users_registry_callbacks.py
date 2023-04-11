ed95923cbe552ed776b7c9850bb5d369d317ab0f
EsojBosonit <joseluis.cuenca@bosonit.com>
2023-04-11 11:04:11 +0200

    mejoras

--- pages/users_registry/users_registry_callbacks.py
+++ pages/users_registry/users_registry_callbacks.py
@@ -4,6 +4,8 @@ from dash import callback
 from dash.dependencies import Input, Output, State
 from pandas import read_csv
 
+from dash.exceptions import PreventUpdate
+
 id_page = "users_registry"
 
 @callback(Output(f"{id_page}_reg_answer", "children"),
@@ -11,10 +13,15 @@ id_page = "users_registry"
           [
             State(f"{id_page}_reg_user", "value"),
             State(f"{id_page}_reg_pass", "value") 
-          ])
+          ], prevent_initial_call=True)
 def auth_display(n_clicks, reg_user, reg_pass):
-    reg_answer = ""
-    if n_clicks:
+    
+
+    if reg_user is None or reg_pass is None:
+        reg_answer = "Faltan Datos"
+    elif len(reg_user) < 1 or len(reg_pass) < 1:
+        reg_answer = "Faltan Datos"
+    else:
         df = read_csv("users.csv")        
         if reg_user in list(df["user"]):
             reg_answer = "El Usuario ya existe"
@@ -23,4 +30,4 @@ def auth_display(n_clicks, reg_user, reg_pass):
             df.loc[df.shape[0]] = [reg_user, reg_pass]
             df.to_csv("users.csv", index=False)
             reg_answer =  "Registro completado"
-    return [reg_answer] 
+    return reg_answer
