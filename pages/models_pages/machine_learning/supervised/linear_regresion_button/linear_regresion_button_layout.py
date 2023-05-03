57a846be7addc65c95040f2d444c3018b14aca8e
EsojBosonit <joseluis.cuenca@bosonit.com>
2023-05-03 06:22:12 +0200

    se corrige identacion

--- pages/models_pages/machine_learning/supervised/linear_regresion_button/linear_regresion_button_layout.py
+++ pages/models_pages/machine_learning/supervised/linear_regresion_button/linear_regresion_button_layout.py
@@ -10,30 +10,36 @@ id_page = "linear"
 
 params_utils = [                
                 html.H5("Params", style=style_title_params),                 
-                create_param_drop(id_page, "fit_intercept", ["True", "False"], "True"),                
-                create_param_drop(id_page, "copy_X", ["True", "False"], "True"),
+                create_param_drop(id_page, "fit_intercept",
+                                  ["True", "False"], "True"),                
+                create_param_drop(id_page, "copy_X",
+                                  ["True", "False"], "True"),
                 create_param_input(id_page, "n_jobs", None),
-                create_param_drop(id_page, "positive", ["True", "False"], "False"),
+                create_param_drop(id_page, "positive",
+                                  ["True", "False"], "False"),
                ]
 
 content_down = my_div(style_content, "",
                       [
-                       
-                               my_div(style_div_target, f"{id_page}_div_derecha",
-                                      [
-                                      my_div(style_title_target, "", html.A("Target")),
-                                      my_div(style_selector_target, "",
-                                            my_dropdown(f"{id_page}_target",
-                                                         {"background": background_in_dropdown},
-                                                         placeholder="Select Target"
-                                            ),
-                                     ),                       
-                                      ]
+                       my_div(style_div_target,
+                              f"{id_page}_div_derecha",
+                              [
+                               my_div(style_title_target, "",
+                                      html.A("Target")
+                               ),
+                               my_div(style_selector_target, "",
+                                      my_dropdown(f"{id_page}_target",
+                                                  {"background": background_in_dropdown},
+                                                  placeholder="Select Target"
+                                      ),
+                               ),                       
+                              ]
                        ),   
                        create_div_split(id_page),   
                        dcc.Loading(
                                id="loading-2",
-                               children=[my_div({"margin-top": "1%"}, f"{id_page}_model_loading")],
+                               children=[my_div({"margin-top": "1%"},
+                                                f"{id_page}_model_loading")],
                                type="default",
                                fullscreen=False,
                        ),
