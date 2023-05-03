758ab59b5d4933cbce21fc1cede418a707500ee7
EsojBosonit <joseluis.cuenca@bosonit.com>
2023-05-03 04:30:49 +0200

    se actualiza con los templates actuales

--- pages/dataframe_pages/view_data_button/view_data_button_callbacks.py
+++ pages/dataframe_pages/view_data_button/view_data_button_callbacks.py
@@ -1,62 +1,30 @@
-from dash import Input, Output, Patch, State, callback, html
+from dash import Input, Output, State, callback, html
 from pandas import read_json
 
 from assets.layout_templates.main_page.common_css import *
-from assets.my_dash.my_dbc.my_button import my_button
+from assets.my_dash.my_html.my_div import my_div
 from utils.create_agGrid import create_adgrid
-from utils.create_callback_button_cover import \
-    create_callback_button_cover
-
-style_boton_files = {  
-    "margin-left": "1%",
-    "margin-bottom": "1%",
-    "width": "13%",
-    "font-size": "0.7vmax",
-    "border": "2px solid",
-    "font-family": font_family,
-    "color": color_boton_1,
-}
+from utils.create_callback_button_cover import create_callback_button_cover
 
 id_page = "view_data"
 
 
+create_callback_button_cover(id_page, f"{id_page}_content_down")
+
 @callback([
-           Output('view_data_content', 'children'),
-           Output('add-data-rows', 'disabled'),
-          ],
+           Output(f"{id_page}_content_down", 'children'),
+           Output(f"{id_page}_loading", "children")
+          ],       
           Input('view_data_button', 'n_clicks'),
           State('main_page_store', 'data'))
 def view_data(n_clicks, data):
 
     obj = []
     try:        
-        df = read_json(data["df"])[0:10]
-        obj.append(
-            [my_button(f"{id_page}_add-data-rows", "View full DataFrame",
-                                   style_boton_files,
-                                   className="btn btn-outline-primary",
-                                   color="black"
-                         ), 
-            create_adgrid(f"{id_page}_ag-table", df)])
+        df = read_json(data["df"])       
         obj.append(False)
     except (TypeError, KeyError, ValueError):
         obj = [html.H6('No hay ningún DataFrame Cargado',
-                       style={"margin-left": "20%", "color": color_boton_1}), True]
-    return obj
-
-
-@callback([
-           Output(f"{id_page}_ag-table", "rowData"),
-           Output(f"{id_page}_add-data-rows", "disabled", allow_duplicate=True),
-          ],
-          Input(f"{id_page}_add-data-rows", "n_clicks"),
-          State("main_page_store", "data"),
-          prevent_initial_call=True,)
-def add_data_to_fig(n_clicks, data):    
-    df = read_json(data["df"])[10:]
-    patched_table = Patch()
-    patched_table.extend(df.to_dict("records"))
-    return [patched_table, True]
-
-
-create_callback_button_cover(id_page)
+                       style={"margin-left": "20%", "color": color_boton_1})]
+    return [my_div({"margin-top": "1%"}, "",
+                   create_adgrid(f"{id_page}_ag-table", df)), ""]
--- pages/dataframe_pages/view_data_button/view_data_button_layout.py
+++ pages/dataframe_pages/view_data_button/view_data_button_layout.py
@@ -1,8 +1,14 @@
-from assets.my_dash.my_html.my_div import my_div
-from pages.dataframe_pages.view_data_button.view_data_button_callbacks import *
+from assets.layout_templates.main_page.content_layout import \
+    create_content_layout
+from assets.layout_templates.panel_params.create_panel_params import *
 
-layout = my_div({"position": "relative", "margin-top": "8%", "margin-left": "2%", "height": "60%"}, "", 
-                [
-                 my_div({"width": "50%"}, "add-data-rows"),
-                 my_div({"margin-top": "1%", "width": "97%"}, "view_data_content")]
-                )
+from .view_data_button_callbacks import *
+
+id_page = "view_data"
+
+content_down = my_div(style_div_content_down, f"{id_page}_content_down")
+
+layout = create_content_layout(id_page,
+                               my_div(style_div_content_up, f"{id_page}_content_up"),
+                               content_down,
+                               my_div(style_div_params, "", ""))
