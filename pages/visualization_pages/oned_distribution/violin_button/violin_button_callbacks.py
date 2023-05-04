7f6d99a6d2dea7f5f50b077b8fa28290f0747331
EsojBosonit <joseluis.cuenca@bosonit.com>
2023-05-04 03:06:13 +0200

    implementado

--- pages/visualization_pages/oned_distribution/violin_button/violin_button_callbacks.py
+++ pages/visualization_pages/oned_distribution/violin_button/violin_button_callbacks.py
@@ -13,9 +13,10 @@ id_page = "violin"
 
 
 create_callback_button_cover(id_page, f"{id_page}_content_down")
-selector_options(id_page, f"{id_page}_X", False)
+selector_options(id_page, f"{id_page}_X")
 selector_options(id_page, f"{id_page}_Y", False)
 selector_options(id_page, f"{id_page}_color")
+selector_options(id_page, f"{id_page}_hover_data")
 
 
 @callback(Output(f"{id_page}_content_up", "children"), 
@@ -27,7 +28,7 @@ def second_callback(n_clicks):
                    html.H5("plotly.express.violin()",
                            style=style_title),
                    html.A("Documentacion",
-                          href="https://plotly.com/python/plotly-express/",
+                          href="https://plotly.com/python/violin/",
                           target="_blank")
                   ])
 
@@ -42,7 +43,11 @@ def second_callback(n_clicks):
            State('main_page_store', 'data'),
            State(f"{id_page}_X", "value"),
            State(f"{id_page}_Y", "value"),
+           State(f"{id_page}_box", 'value'),
+           State(f"{id_page}_points", 'value'),
+           State(f"{id_page}_violinmode", 'value'),
            State(f"{id_page}_color", 'value'),
+           State(f"{id_page}_hover_data", 'value'),
           ],
           prevent_initial_call=True)
 def display_page(
@@ -50,23 +55,37 @@ def display_page(
     data,
     state_X,
     state_Y,
+    state_box,
+    state_points,
+    state_violinmode,
     state_color,
+    state_hover_data,
     ):     
     
     if state_color is not None and len(state_color) < 1 or state_color == " ":
         state_color = None   
-    
-    if state_X and state_Y:
+    if state_hover_data is not None and len(state_hover_data) < 1 or state_hover_data == " ":
+        state_hover_data = None
+    if state_X is not None and len(state_X) < 1 or state_X == " ":
+        state_X = None   
+    if state_points == "False": state_points = False    
+    state_box = False if state_box == "False" else True
 
+    if state_Y:   
+        
         df = read_json(data["df"])
 
-        fig = px.bar(
-            df,
-            template=template_visualizations,
+        fig = px.violin(
+            df,            
             x=state_X,
             y=state_Y,
+            box=state_box,
+            points=state_points,
+            violinmode=state_violinmode,
+            color=state_color,
+            hover_data=state_hover_data,
             height=550,
-            color=state_color, 
+            template=template_visualizations,            
             color_discrete_sequence=sequential.Plasma,  
         ).update_layout(legend={"title_font_color": color_boton_1})   
         return [dcc.Graph(figure=fig), ""]
