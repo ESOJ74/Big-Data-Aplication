3a2f51ca6bf3699582dab4283ba1482460879816
EsojBosonit <joseluis.cuenca@bosonit.com>
2023-05-04 17:24:58 +0200

    se soluciona error al descargar pipeline

--- pages/pipeline_pages/show_pipeline_button/show_pipeline_button_callbacks.py
+++ pages/pipeline_pages/show_pipeline_button/show_pipeline_button_callbacks.py
@@ -1,22 +1,20 @@
+import os
+import subprocess
+
 from dash import callback, html
 from dash.dependencies import Input, Output, State
 from pandas import read_json
-import subprocess
+
 from assets.my_dash.my_html.my_div import my_div
-import os
 
 id_page = "show_pipeline"
 
-@callback(
-    [
-      Output(f"{id_page}_result", "children"),
-    ],
-    Input("show_pipeline_button", "n_clicks"),
-    [ 
-      State("main_page_store", "data"),
-    ],
-    prevent_initial_call=True,)
+@callback(Output(f"{id_page}_result", "children"),
+          Input("show_pipeline_button", "n_clicks"),
+          State("main_page_store", "data"),
+          prevent_initial_call=True,)
 def add_data_to_fig(refres, data):  
+
     df = read_json(data["pipeline"])
    
     with open("output.txt", "w") as file:
@@ -27,9 +25,9 @@ def add_data_to_fig(refres, data):
 
     obj = []
     with open("output.txt", "r") as file:
-        for line in file:
-         
+        for line in file:         
             obj.append(html.P(line, style={"margin-bottom": "0%","white-space": "pre-wrap"}))
+            
     os.remove("output.txt")
      
     return [my_div({"margin-left": "3%", "margin-top": "10%"}, "", obj)]
