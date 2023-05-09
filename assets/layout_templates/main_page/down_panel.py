8bb1cc7c69806799425d28cd08e7ce6c055d9ad0
EsojBosonit <joseluis.cuenca@bosonit.com>
2023-05-09 09:21:45 +0200

    se añade chatgpt y phind

--- assets/layout_templates/main_page/down_panel.py
+++ assets/layout_templates/main_page/down_panel.py
@@ -5,15 +5,37 @@ def down_panel():
     return my_div(
         style_down_panel,
         "",
-        my_div(
-            {"margin-left": "2%"},
-            "",
-            html.A(
-                "GitHub",
-                href="https://github.com/ESOJ74/Big-Data-Aplication",
-                style={"color": "black"},
-                target="_blank",
+        [
+            my_div(
+                {"float": "left", "margin-left": "2%"},
+                "",
+                html.A(
+                    "GitHub",
+                    href="https://github.com/ESOJ74/Big-Data-Aplication",
+                    style={"color": "black"},
+                    target="_blank",
+                ),
             ),
-        ),
+            my_div(
+                {"float": "left", "margin-left": "2%"},
+                "",
+                html.A(
+                    "ChatGPT",
+                    href="https://chat.openai.com/",
+                    style={"color": "black"},
+                    target="_blank",
+                ),
+            ),
+            my_div(
+                {"float": "left", "margin-left": "2%"},
+                "",
+                html.A(
+                    "Phind",
+                    href="https://www.phind.com/",
+                    style={"color": "black"},
+                    target="_blank",
+                ),
+            ),
+        ],
         className="alert-primary",
     )
