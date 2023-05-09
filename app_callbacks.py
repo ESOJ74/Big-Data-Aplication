af579805a1ca67de5bd8b9e8704d27f996d37bf4
EsojBosonit <joseluis.cuenca@bosonit.com>
2023-05-09 08:53:21 +0200

    .

--- app_callbacks.py
+++ app_callbacks.py
@@ -5,13 +5,12 @@ import pages
 
 lista_de_paginas = {
     "/": pages.main_page.main_page_layout.layout,
-    "/registro": pages.users_registry.users_registry_layout.layout
+    "/registro": pages.users_registry.users_registry_layout.layout,
 }
 
 
 # Update page content
-@callback(Output("app_content", "children"),
-          Input("url", "pathname"))
+@callback(Output("app_content", "children"), Input("url", "pathname"))
 def display_page(pathname):
     if pathname in lista_de_paginas:
-        return lista_de_paginas[pathname]    
+        return lista_de_paginas[pathname]
